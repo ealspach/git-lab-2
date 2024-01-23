e94f78d3f9cc549a805de8580b7b9f65a99f7ffd
ealspach <ea749122@ohio.edu>
2024-01-23 12:05:27 -0500

    New program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Evan Alspach
+ *        Date: 1/23/2024
+ * Description: This is a program for the git lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
